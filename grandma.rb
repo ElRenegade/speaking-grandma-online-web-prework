b3df14ed9a1d12f36998263a2d478d0046bbb638
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-09 04:06:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma(phrase)# Write a speak_to_grandma method.
-  if phrase == phrase.upcase
-    response1 = "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     response2 = "I LOVE YOU TOO PUMPKIN!"
+
+  elsif phrase == phrase.upcase
+  response1 = "NO, NOT SINCE 1938!"
 
   else
     response3 = "HUH?! SPEAK UP, SONNY!"
